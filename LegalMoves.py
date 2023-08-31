82d695d598456e9f37550c84be4998dc082fc1b8
Ritik <flipit001@gmail.com>
2023-08-30 21:36:42 -0700

    deprecated version

--- LegalMoves.py
+++ LegalMoves.py
@@ -2,7 +2,7 @@ from vars import *
 
 opposites = {"w": "b", "b": "w"}
 
-def isLegalMove(board, piece, move_coords: tuple):
+def isLegalMove(board, start_pos: tuple, move_coords: tuple):
 
     move_sqare = board[move_coords[0]][move_coords[1]]
 
