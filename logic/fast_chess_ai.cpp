8cab19feb23b658440b2af73b26187e5b6648443
Ritik <flipit001@gmail.com>
2023-10-28 18:32:31 -0700

    added pawn_moves, and added functionality to print bitboards

--- logic/fast_chess_ai.cpp
+++ logic/fast_chess_ai.cpp
@@ -1,13 +1,14 @@
 #include <stdint.h>
 #include <string>
 #include <iostream>
+#include <vector>
 
 typedef uint64_t BB; // short for Bitboard
 
 
 // constants
 const BB A_FILE = 0x8080808080808080ULL; // represent a file
-const BB RANK_1 = 0xFF00000000000000ULL; // represent first rank
+const BB RANK_1 = 0x00000000000000FFULL; // represent first rank
 const std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"; // starting fen
 
 // bitwise functions
@@ -23,6 +24,10 @@ BB get_bit(BB bitboard, int index) {
     return bitboard & (1ULL << index);
 }
 
+BB shift_back(BB bitboard, int back) {
+    return back >= 0 ? (bitboard << back): (bitboard >> -back);
+}
+
 // bitboards
 BB empties;
 BB white;
@@ -33,6 +38,25 @@ BB rooks;
 BB bishops;
 BB knights;
 BB pawns;
+
+void print_BB(BB bitboard) {
+    for (int i = 63; i >= 0; i--) {
+        int cur = bitboard >> i;
+
+        if ((i+1) % 8 == 0) {
+            std::cout << std::endl;
+        }
+
+        if (cur & 1) {
+            std::cout << "1";
+        }
+
+        else {
+            std::cout << "0";
+        }
+    }
+    std::cout << std::endl;
+}
 
 void open_fen(std::string fen) {
     char cur;
@@ -83,8 +107,26 @@ void open_fen(std::string fen) {
     empties = ~(white | black);
 }
 
+
+
+// direction should be 8 (down), or -8 (up) for
+BB pawn_moves(BB pawns_to_move, BB opposite_team, int direction) { // TODO: add en passent
+
+    BB single_moves = shift_back(pawns_to_move, direction) & empties;
+
+    BB double_moves = shift_back(single_moves, direction) & empties;
+
+    BB captures = (shift_back(pawns_to_move, direction+1) | shift_back(pawns_to_move, direction-1)) & opposite_team;
+
+    return single_moves | double_moves | captures;
+}
+
+// BB knight_moves(BB knights_to_move) {
+//     ;
+// }
+
 int main() {
     open_fen(starting_fen);
-    std::cout << empties << std::endl;
+    print_BB(pawn_moves(pawns & black, white, 8));
     return 0;
 }
