907bbe969b685e1f89d369776a35a6a55cfa819d
flipit001 <flipit001@gmail.com>
2024-02-16 18:40:43 -0800

    moved board to its own file

--- logic/fast_chess_ai.cpp
+++ logic/fast_chess_ai.cpp
@@ -6,78 +6,83 @@
 #include "non_sliding_moves.hpp"
 #include "moveboards.hpp"
 #include "magic_values.hpp"
+#include "board.hpp"
 
 
 // const std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"; // starting fen
-const std::string starting_fen = "r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1";
+// const std::string starting_fen = "r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1";
 
 // bitboards
-BB empties;
-BB white;
-BB black;
-BB kings;
-BB queens;
-BB rooks;
-BB bishops;
-BB knights;
-BB pawns;
+// BB empties;
+// BB white;
+// BB black;
+// BB kings;
+// BB queens;
+// BB rooks;
+// BB bishops;
+// BB knights;
+// BB pawns;
 
-void open_fen(std::string fen) {
-    char cur;
-    int board_ptr = 0;
-    for (int i = fen.length(); i > 0; i--)
-    {
-        cur = fen[i-1];
+// void open_fen(std::string fen) {
+//     char cur;
+//     int board_ptr = 0;
+//     for (int i = fen.length(); i > 0; i--)
+//     {
+//         cur = fen[i-1];
 
-        if (isdigit(cur))
-        {
-            // skip squares
-            board_ptr += std::stoi(&cur);
-        }
-        else if (cur == '/')
-        {
-            // next row
-            continue;
-        }
-        else
-        {
-            // // place piece
-            // board[board_ptr] = (islower(cur) ? Black : White) | (fen_to_piece[std::tolower(cur)]);
-            // board_ptr++;
-            isupper(cur) ? set_bit_on(white, board_ptr) : set_bit_on(black, board_ptr);
-            cur = char(std::tolower(cur));
-            if (cur == 'k') {
-                set_bit_on(kings, board_ptr);
-            }
-            else if (cur == 'q') {
-                set_bit_on(queens, board_ptr);
-            }
-            else if (cur == 'r') {
-                set_bit_on(rooks, board_ptr);
-            }
-            else if (cur == 'b') {
-                set_bit_on(bishops, board_ptr);
-            }
-            else if (cur == 'n') {
-                set_bit_on(knights, board_ptr);
-            }
-            else if (cur == 'p') {
-                set_bit_on(pawns, board_ptr);
-            }
+//         if (isdigit(cur))
+//         {
+//             // skip squares
+//             board_ptr += std::stoi(&cur);
+//         }
+//         else if (cur == '/')
+//         {
+//             // next row
+//             continue;
+//         }
+//         else
+//         {
+//             // // place piece
+//             // board[board_ptr] = (islower(cur) ? Black : White) | (fen_to_piece[std::tolower(cur)]);
+//             // board_ptr++;
+//             isupper(cur) ? set_bit_on(white, board_ptr) : set_bit_on(black, board_ptr);
+//             cur = char(std::tolower(cur));
+//             if (cur == 'k') {
+//                 set_bit_on(kings, board_ptr);
+//             }
+//             else if (cur == 'q') {
+//                 set_bit_on(queens, board_ptr);
+//             }
+//             else if (cur == 'r') {
+//                 set_bit_on(rooks, board_ptr);
+//             }
+//             else if (cur == 'b') {
+//                 set_bit_on(bishops, board_ptr);
+//             }
+//             else if (cur == 'n') {
+//                 set_bit_on(knights, board_ptr);
+//             }
+//             else if (cur == 'p') {
+//                 set_bit_on(pawns, board_ptr);
+//             }
 
-            board_ptr++;
-        }
-    }
-    empties = ~(white | black);
-}
+//             board_ptr++;
+//         }
+//     }
+//     empties = ~(white | black);
+// }
 
 
 int main() {
-    init_moveboards();
+    // init_moveboards();
+    Board chess = Board(DEFAULT_FEN);
+    print_BB(chess.pieces[KNIGHT] & chess.pieces[BLACK]);
     
-    open_fen(starting_fen); // 51
-    print_BB(black | white);
-    print_BB(BISHOP_MASKS[51]);
+    
+    // open_fen(starting_fen); // 51
+    // print_BB(black | white);
+    // print_BB(BISHOP_MASKS[51]);
+    // print_BB(blocker_mask_bishop(51));
     // print_BB(BISHOP_MOVES[51][calculate_index((black | white) & BISHOP_MASKS[51], BISHOP_MAGICS[51], BISHOP_SHIFTS[51])]);
     // std::vector<BB> blocker_board = blocker_boards(blocker_mask_rook(23));
     // for (int i = 0; i < blocker_board.size(); i++) {
