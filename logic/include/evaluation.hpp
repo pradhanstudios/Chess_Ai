49e5348ee5728726a2a0fd1d0e362c80c8bf4543
flipit001 <flipit001@gmail.com>
2024-04-19 13:20:45 -0700

    added a very basic search and very basic eval function

---
+++ logic/include/evaluation.hpp
@@ -0,0 +1,11 @@
+#ifndef EVALUATION_HPP
+#define EVALUATION_HPP
+
+#include "constants.hpp"
+#include "bit_operations.hpp"
+#include "board.hpp"
+
+inline std::array<int, 7> piece_values = {0, 10, 33, 35, 50, 90, 0};
+int simple_eval(const Board &chess_board);
+
+#endif // EVALUATION_HPP
---
+++ logic/src/evaluation.cpp
@@ -0,0 +1,17 @@
+#include "evaluation.hpp"
+
+int simple_eval(const Board &chess_board) {
+    int eval = 0;
+    eval += real_count(chess_board.pieces[PAWN] & chess_board.pieces[WHITE]) * piece_values[PAWN];
+    eval += real_count(chess_board.pieces[KNIGHT] & chess_board.pieces[WHITE]) * piece_values[KNIGHT];
+    eval += real_count(chess_board.pieces[BISHOP] & chess_board.pieces[WHITE]) * piece_values[BISHOP];
+    eval += real_count(chess_board.pieces[ROOK] & chess_board.pieces[WHITE]) * piece_values[ROOK];
+    eval += real_count(chess_board.pieces[QUEEN] & chess_board.pieces[WHITE]) * piece_values[QUEEN];
+    eval -= real_count(chess_board.pieces[PAWN] & chess_board.pieces[BLACK]) * piece_values[PAWN];
+    eval -= real_count(chess_board.pieces[KNIGHT] & chess_board.pieces[BLACK]) * piece_values[KNIGHT];
+    eval -= real_count(chess_board.pieces[BISHOP] & chess_board.pieces[BLACK]) * piece_values[BISHOP];
+    eval -= real_count(chess_board.pieces[ROOK] & chess_board.pieces[BLACK]) * piece_values[ROOK];
+    eval -= real_count(chess_board.pieces[QUEEN] & chess_board.pieces[BLACK]) * piece_values[QUEEN];
+
+    return eval;
+}
